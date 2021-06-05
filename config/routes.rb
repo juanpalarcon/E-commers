91f48976f774cbabe080f83c9d505bc89787f6a7
Juan Pablo <juanpablogast@gmail.com>
2021-06-05 19:45:35 -0400

    agregando devise y protegiendo product y category

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :categories
+
+  devise_for :admins
+  authenticate :admin do 
+    resources :products
+    resources :categories
+  
+  end
+
+  root to: "home#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
