9276d7453dd6cdb65480b0616427649ecc611748
Juan Pablo <juanpablogast@gmail.com>
2021-06-09 19:53:32 -0400

    agregando show a cart y logican en el controller carts

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,10 +2,14 @@ class CartsController < ApplicationController
     before_action :authenticate_user!
 
     def update
-        
+        product_id = params[:cart][:product_id]
+        quantity = params[:cart][:quantity]
+
+        current_order.add_product(product_id, quantity)
+        redirect_to root_path, notice: 'Product added succesfuly'
     end
 
     def show
-        
+        @order = current_order
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'home/index'
   devise_for :users
   devise_for :admins
   authenticate :admin do 
     resources :products
     resources :categories
-  
   end
 
-  root to: "products#index"
+  resource :cart, only: [:show, :update]
+
+  root to: "home#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
